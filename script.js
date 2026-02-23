c711b128bfdcac0742d83bd502fdaf15ec00c4f2
Inchara nk <incharank1@gmail.com>
2026-02-23 11:51:28 +0530

    updated

---
+++ script.js
@@ -0,0 +1,56 @@
+console.log("Lets write js")
+
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML=response;
+    let as= div.getElementsByTagName("a")
+
+    let songs=[]
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1])
+        }
+    }
+    return songs
+}
+
+async function main(){
+    //get the list of all songs
+let songs= await getSongs()
+console.log(songs)
+
+let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    songUL.innerHTML = ""
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="img/music.svg" alt="">
+                            <div class="info">
+                                <div> ${song.replaceAll("%20", " ")}</div>
+                                <div>Inchara</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="img/play.svg" alt="">
+                            </div> </li>`;
+    }
+    // Attach an event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+
+        })
+    })
+
+    return songs
+
+
+
+    //play the first song
+    var audio = new Audio(songs[0]);
+    audio.play();
+
+}
+main()
